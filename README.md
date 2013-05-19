e065974bbea8f7fdc0192714c379cf636c0bddd9
Diego Escalante Urrelo <diegoe@gnome.org>
2013-05-18 21:39:05 -0500

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-twitrunner
-==========
+TwitRunner
+=========
 
-Extra simple Twitter data miner
+This is a handy script to extract the number of twitter followers and
+some additional information based on a .txt file.
+
+I wrote it to mine data of teachers using twitter.
+
+How to use it
+-------------
+You have to fill keys.txt with your Twitter developer (application)
+tokens and users.txt with the ids and extra data you want to include.
+
+Hopefully I'll clean it up to be more generic.
+
+Todo
+----
+* Make users.txt self documented, somehow.
+* Add sorting of results.
+* Add a timestamp to results.
+
+Contribute
+----------
+Although an extra simple, one-use tool, I'm happy to apply patches and
+suggestions to make this more useful.
+Feel free to fork me, or just mail me at diegoe@gnome.org.
---
+++ twitrunner.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# twitrunner - an extra simple twitter miner
+#
+# Author: Diego Escalante Urrelo <diegoe@gnome.org>
+# URL: https://github.com/diegoe/twitrunner
+#
+# A very, very, simple data miner for twitter.
+
+import codecs
+import tweepy
+
+# keys.txt is a file made up of 4 lines:
+# consumer_key
+# consumer_secret
+# consumer_token
+# consumer_token_secret
+#
+# You need to get this keys from your twitter developer account:
+# https://dev.twitter.com/apps
+
+keys = open("keys.txt").readlines()
+
+consumer_key = keys[0].strip()
+consumer_secret = keys[1].strip()
+
+access_token = keys[2].strip()
+access_token_secret = keys[3].strip()
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+print "Authenticated as: %s (%s)" % (api.me().name, api.me().screen_name)
+
+with codecs.open("users.txt", "r", "utf-8") as f:
+    users = f.readlines()
+
+for teacher in users:
+    name, userid, university, topics = teacher.split("\t")
+
+    try:
+        p = api.get_user(userid)
+        print "%s\t%s\t%s\t%s\thttps://twitter.com/%s" % \
+            (name, university, topics.strip(), p.followers_count, userid)
+    except:
+        print "@id is not a valid twitter ID"
